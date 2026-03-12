826d66bfbae86740a32767f53caa76ecdfeaa52b
desislavaslavova24-oss <desislava.slavova.24@trakia-uni.bg>
2026-03-12 16:22:51 +0200

    Add Song class with constructor and properties

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-// JavaScript will be added in next commits
-console.log('Music Playlist App initialized');
+class Song {
+    constructor(name, artist, genre, format, rating, hasVideo) {
+        this.name = name;
+        this.artist = artist;
+        this.genre = genre;
+        this.format = format;
+        this.rating = rating;
+        this.hasVideo = hasVideo;
+        this.id = Date.now();
+        this.dateAdded = new Date();
+    }
+}
+
+console.log('Song class created');
